813af77a1d6bcbe413050cc00526e66e99c7fa9a
naolumer <naolumer3@gmail.com>
2025-04-10 21:40:31 +0300

    added year filed in the movie model

--- models/Movies.js
+++ models/Movies.js
@@ -14,6 +14,7 @@ const movieSchema = new mongoose.Schema({
     comments: [
         {value: {type: String}, published: {type:Date,default:Date.now}},
     ],
+    year: {type:Date, default:Date.now()}
 });
 
 // Creating Model
